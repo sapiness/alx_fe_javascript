438dbb8231bc4ff320f6e07f12e6414141db88db
Surprise <sapiness695@gmail.com>
2024-10-19 11:41:16 +0200

    test3

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -1,29 +1,31 @@
-// document.addEventListener("DOMContentLoaded",showRandomQuote);
 
-const createAddQuoteForm = document.getElementById("AddQuoteForm");
+const form = document.getElementById("AddQuoteForm");
 const button = document.getElementById("newQuote");
 const quoteDisplay = document.getElementById("quoteDisplay");
 
-let quotes = [];
- function addQuote(){
-  const text = document.getElementById("newQuoteText").value;
-  const category = document.getElementById("newQuoteCategory").value; 
 
+const quotes = [/*{text : "love what you do", category : "Hardwork"}*/];
+            
+function addQuote(){
+const text = document.getElementById("newQuoteText").value;
+const category = document.getElementById("newQuoteCategory").value; 
 
-let object = {text: text, category: category}
+let object = {text : text, category : category};
 quotes.push(JSON.stringify(object));
+
 
  }
 
-button.addEventListener("click", function showRandomQuote(){
-  quoteDisplay.innerHTML = quotes;
-showRandomQuote = quotes[Math.floor(Math.random() * quotes.length)]
-   
+button.addEventListener("click", function showRandomQuote(e){
+  const paragraph = document.createElement('p');
+  quoteDisplay.appendChild(paragraph);
+
+e = Math.floor(Math.random() * quotes.length)
+console.log(quotes[e]); 
+paragraph.innerHTML = quotes[e];
 }
 
 );
-createAddQuoteForm.addEventListener("submit", function(e){
+form.addEventListener("submit", function(e){
    e.preventDefault();    
 });
-
-
