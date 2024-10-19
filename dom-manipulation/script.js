2a9e5925f02f6b6fbbc1ca98077b3dcf9aac7c5e
Surprise <sapiness695@gmail.com>
2024-10-19 11:47:50 +0200

    Implementing Web Storage and JSON Handling

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -29,3 +29,14 @@ paragraph.innerHTML = quotes[e];
 createAddQuoteForm.addEventListener("submit", function(e){
    e.preventDefault();    
 });
+
+function importFromJsonFile(event) {
+  const fileReader = new FileReader();
+  fileReader.onload = function(event) {
+    const importedQuotes = JSON.parse(event.target.result);
+    quotes.push(...importedQuotes);
+    saveQuotes();
+    alert('Quotes imported successfully!');
+  };
+  fileReader.readAsText(event.target.files[0]);
+}
