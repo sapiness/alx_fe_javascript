d11d90f783fc8dd380873e1cf00b734a76d34eed
Surprise <sapiness695@gmail.com>
2024-10-19 11:43:43 +0200

    test4

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -1,5 +1,5 @@
 
-const form = document.getElementById("AddQuoteForm");
+const createAddQuoteForm = document.getElementById("AddQuoteForm");
 const button = document.getElementById("newQuote");
 const quoteDisplay = document.getElementById("quoteDisplay");
 
@@ -26,6 +26,6 @@ paragraph.innerHTML = quotes[e];
 }
 
 );
-form.addEventListener("submit", function(e){
+createAddQuoteForm.addEventListener("submit", function(e){
    e.preventDefault();    
 });
